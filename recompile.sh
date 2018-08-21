c96fdcd0ce3d71b44b8d5a96548a3e701b267815
Anthony Kalaitzis <anthony.kalaitzis@gmail.com>
2018-08-21 17:14:52 +0930

    Updated module files, documentation and removed personal recompile

--- recompile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ifort -O2 -c kinds.f90
-ifort -O2 -mkl -c lapack.f90
-ifort -O2 -c PLplots.f90 -I/home/anthony/bin/PLplot/install_dir/include/plplot -I/home/anthony/bin/PLplot/install_dir/lib/fortran/modules/plplot -L/home/anthony/bin/PLplot/install_dir/lib -lplplotfortran -lplplot
-ifort -O2 -mkl  -c numFort.f90 -lmkl_lapack95_lp64 
