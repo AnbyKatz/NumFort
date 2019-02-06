6d4243de4dc987cfcb4d3a9c596939f5afcedbd7
Anthony Kalaitzis <anthony.kalaitzis@adelaide.edu.au>
2019-02-06 11:11:30 +1030

    adding new PLplot branch

--- install.sh
+++ install.sh
@@ -25,31 +25,12 @@ chmod +x recompile.sh
 PLplotPath="/path/to/PLplotInstall"
 
 echo ""
-echo "did you install PLplot/Want to use shite plotting?"
-echo "Please enter y or n"
-read stringIn
-while [ "$stringIn" != "y" ] &&  [  "$stringIn" != "n" ]
-do
-    echo "did you install PLplot?"
-    echo "Please enter y or n"
-    read stringIn
-done
-if [ "$stringIn" == "y" ]
-then
-    echo "Please input your path to PLplot in the form"
-    echo "/path/to/PLplotInstallDirectory"
-    read PLplotPath
-fi
+echo "Please input your path to PLplot in the form"
+echo "/path/to/PLplotInstallDirectory"
+read PLplotPath
 
 touch makefile
 cat .makeTemp >> makefile
-
-if [ "$stringIn" == "y" ]
-then    
-    rm makefile
-    touch makefile
-    cat .makeTempPL >> makefile
-fi
 
 cwd=$(pwd)
 echo "DIR =" $cwd/ | cat - makefile > temp && mv temp makefile
@@ -67,11 +48,7 @@ echo "#!/bin/bash" >> recompile.sh
 echo "ifort -O2 -c kinds.f90" >> recompile.sh
 echo "ifort -O2 -mkl -c lapack.f90" >> recompile.sh
 echo "ifort -O2 -c minf.f90" >> recompile.sh
-if [ "$stringIn" == "y" ]
-then    
 echo "ifort -O2 -c PLplots.f90 -I$PLplotPath/include/plplot -I$PLplotPath/lib/fortran/modules/plplot -L$PLplotPath/lib -lplplotfortran -lplplot" >> recompile.sh
-fi
-
 echo "ifort -O2 -mkl -c numFort.f90 -lmkl_lapack95_lp64" >> recompile.sh
 echo "Compiling numFort library, may take a bit"
 echo ""
