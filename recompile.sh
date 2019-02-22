7cdf58381996282f7b2084d3f576ab7eb2738a87
Anthony Kalaitzis <anthony.kalaitzis@adelaide.edu.au>
2019-02-22 17:37:41 +1030

    added progress bar

--- recompile.sh
+++ recompile.sh
@@ -4,9 +4,32 @@ rm -rf Build
 mkdir Build
 
 cwd=$(pwd)/src
+
 cd Build
+
+ncols=$(tput cols)
+barwidth=$((ncols/4))
+
+echo -n '|'
 ifort -O2 -c $cwd/kinds.f90
+for ((i=1;i<=$barwidth;i++))
+do
+    echo -n '='    
+done
 ifort -O2 -mkl -c $cwd/lapack.f90
+for ((i=1;i<=$barwidth;i++))
+do
+    echo -n '='    
+done
 ifort -O2 -c $cwd/minf.f90
+for ((i=1;i<=$barwidth;i++))
+do
+    echo -n '='    
+done
 ifort -O2 -mkl -c $cwd/numFort.f90 -lmkl_lapack95_lp64
+for ((i=1;i<=$barwidth;i++))
+do
+    echo -n '='    
+done
+echo '|'
 cd ..
