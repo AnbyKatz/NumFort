6e28fccd4b3b44d925a178431fa604dce1719319
Curtis Abell <curtis.abell@adelaide.edu.au>
2020-10-23 15:21:54 +1030

    Added Quad Precision real kind

--- src/kinds.f90
+++ src/kinds.f90
@@ -1,8 +1,12 @@
 module kinds
   implicit none
 
+  ! Real Kinds
   integer,  parameter :: SP = kind(1.0)
   integer,  parameter :: DP = kind(1.0D0)
+  integer,  parameter :: QP = kind(1.0Q0)
+
+  ! Constants
   real(DP), parameter :: PI = 3.141592653589793238462643383279502884197_dp
   real(DP), parameter :: Infty = huge(1.0_DP)
 
