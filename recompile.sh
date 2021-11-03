8d099988e6cb298cbb02aa0e4f118d9ec63f6cd0
Curtis Abell <curtisabell97@gmail.com>
2021-11-03 13:53:28 +1030

    Change mkl flag to qmkl in line with new standards

--- recompile.sh
+++ recompile.sh
@@ -14,27 +14,27 @@ echo -n '|'
 ifort -O2 -c $cwd/kinds.f90
 for ((i=1;i<=$barwidth;i++))
 do
-    echo -n '='    
+    echo -n '='
 done
-ifort -O2 -mkl -c $cwd/lapack.f90
+ifort -O2 -qmkl -c $cwd/lapack.f90
 for ((i=1;i<=$barwidth;i++))
 do
-    echo -n '='    
+    echo -n '='
 done
 ifort -O2 -c $cwd/minf.f90
 for ((i=1;i<=$barwidth;i++))
 do
-    echo -n '='    
+    echo -n '='
 done
 ifort -O2 -c $cwd/quadpack.f90
 for ((i=1;i<=$barwidth;i++))
 do
-    echo -n '='    
+    echo -n '='
 done
-ifort -O2 -mkl -c $cwd/NumFort.f90 -lmkl_lapack95_lp64
+ifort -O2 -qmkl -c $cwd/NumFort.f90 -lmkl_lapack95_lp64
 for ((i=1;i<=$barwidth;i++))
 do
-    echo -n '='    
+    echo -n '='
 done
 echo '|'
 cd ..
